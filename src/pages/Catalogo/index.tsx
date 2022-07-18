c239181c60ada2fe3d04717705081a5546e0d6de
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-07-18 11:11:19 -0300

    cartao de produto finalizado e responsivo

--- src/pages/Catalogo/index.tsx
+++ src/pages/Catalogo/index.tsx
@@ -5,19 +5,19 @@ export function Catalogo() {
     <div className="container my-4 ">
       <h1>Botao de pesquisa</h1>
       <div className="row">
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
           <ProductCard />
         </div>
       </div>
